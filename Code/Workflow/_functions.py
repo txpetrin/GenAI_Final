20917b680f9e398aea03923ffbc9c5fa127101d4
txpetrin <txpetrin@mtu.edu>
2025-02-22 18:54:47 -0500

    Application Finished

--- Code/Workflow/_functions.py
+++ Code/Workflow/_functions.py
@@ -1,6 +1,14 @@
 from _setup import *
 import requests
 from datetime import datetime, timedelta
+from statsforecast import StatsForecast
+
+from neuralforecast import NeuralForecast
+from neuralforecast.models import NBEATS, NHITS
+from neuralforecast.utils import AirPassengersDF
+
+from statsforecast.models import ARIMA
+
 
 # docs
 # https://polygon.io/docs/stocks/get_v2_aggs_ticker__stocksticker__prev
@@ -8,7 +16,7 @@ from datetime import datetime, timedelta
 client = RESTClient()  # POLYGON_API_KEY environment variable is used
 
 # Function for use in retreiving stock data
-def get_stock_data(ticker = "GM", from_=(datetime.today() - timedelta(weeks=6)).strftime('%Y-%m-%d'), to=datetime.today().strftime('%Y-%m-%d')):
+def get_stock_data(ticker = "GM", from_=(datetime.today() - timedelta(weeks=26)).strftime('%Y-%m-%d'), to=datetime.today().strftime('%Y-%m-%d')):
 
     # List Aggregates (Bars)
     aggs = []
@@ -16,22 +24,30 @@ def get_stock_data(ticker = "GM", from_=(datetime.today() - timedelta(weeks=6)).
         aggs.append(a)
 
     aggs = pd.DataFrame(aggs)
-    returnee = pd.DataFrame(aggs['open'])
     return aggs
 
-# TODO : Implement basic forecast here
+def get_arima_predictions(data: pd.DataFrame, h=8, ticker="GM") -> pd.DataFrame:
+    # Instantiate the StatsForecast object with ARIMA and AutoETS models.
+    # We assume a daily frequency ('D'); adjust as needed.
 
-# TODO : Create this if there is time
-# def get_options_data(ticker = "GM", timespan = "day", from_=(datetime.today() - timedelta(weeks=2)).strftime('%Y-%m-%d'), to=datetime.today().strftime('%Y-%m-%d')):
-#     client = RESTClient()  # POLYGON_API_KEY environment variable is used
-# 
-#     options_chain = []
-#     for o in client.list_snapshot_options_chain(
-#         "HCP",
-#         params={
-#             "expiration_date.gte": "2024-03-16",
-#             "strike_price.gte": 29,
-#             "strike_price.lte": 30,
-#         },
-#     ):
-#         options_chain.append(o)
+    df = data.rename(columns={'close': 'y', 'date': 'ds'})
+    df['unique_id'] = ticker
+
+    sf = StatsForecast(models=[ARIMA(order=(4,1,4))], freq='D')
+    # Fit the model(s) on the historical data
+    sf.fit(df)
+
+    # Generate forecasts
+    forecasts = sf.forecast(df=df, h=h, fitted=True)
+
+    models = [NBEATS(input_size=2*h, h=h, max_steps=100, enable_progress_bar=False),
+          NHITS(input_size=2*h, h=h, max_steps=100, enable_progress_bar=False)]
+    nf = NeuralForecast(models=models, freq='D')
+    
+    nf.fit(df)
+    Y_hat_df = nf.predict()
+
+    forecasts = forecasts.drop(columns=['ds'])
+    returnee = pd.concat([forecasts.rename(columns={'y': 'ARIMA'}), Y_hat_df], axis=1)
+
+    return returnee
